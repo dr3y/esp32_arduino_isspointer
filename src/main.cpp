031a05a87d6d3bf580f16e0048a66233906986dd
Andrey Shur <andrey.shur@gmail.com>
2020-01-24 10:59:37 -0800

    stepper tests

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
 #include <TinyGPS++.h>
+//#include <SoftwareSerial.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -14,21 +15,23 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 TinyGPSPlus gps;
 #define EEPROM_SIZE 1024
-typedef struct wifilogin {
-  char ssid[16];
-  char pwd[16];
-} WIFILOGIN;
-
-WIFILOGIN wflogin;
-
+HardwareSerial SerialGPS(1);
+//SoftwareSerial gpsserial(39,34);
+#define s1dir 23
+#define s1step 36
 void setup() {
+  pinMode(s1dir,OUTPUT);
+  pinMode(s1step,OUTPUT);
+  digitalWrite(s1dir,HIGH); //dir for stepper
+  digitalWrite(s1step,LOW); //step for stepper
+  int retry=0;
   Serial.begin(9600);
-  Serial2.begin(4800);
+  SerialGPS.begin(9600,SERIAL_8N1,16,17);
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
-  Serial.println("succes!");
+  Serial.println("success!");
   display.display();
 
   vTaskDelay(1000); // Pause for 2 seconds
@@ -40,93 +43,75 @@ void setup() {
   
 
   /* Set ESP32 to WiFi Station mode */
-  WiFi.mode(WIFI_AP_STA);
-  /* start SmartConfig */
-  WiFi.beginSmartConfig();
-
-  /* Wait for SmartConfig packet from mobile */
-  display.println(F("Waiting for SmartConfig."));
+  WiFi.mode(WIFI_STA);
+  WiFi.begin();
+  display.println("Attempt to connect to WiFi network...");
   display.display();
-  while (!WiFi.smartConfigDone()) {
-    delay(500);
-    display.print(F("."));
+  while(WiFi.status() != WL_CONNECTED) {
+    display.print(".");
     display.display();
-  }
-  display.println(F(""));
-  display.println(F("SmartConfig done."));
-
-  /* Wait for WiFi to connect to AP */
-  display.println(F("Waiting for WiFi"));
-  display.display();
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    display.print(F("."));
-    display.display();
-  }
-  display.println(F("WiFi Connected."));
-  display.print(F("IP Address: "));
-  display.println(WiFi.localIP());
-  display.display();
-  delay(2000); // Pause for 2 seconds
-  }
-void loop() {
-  display.clearDisplay();
-  display.setTextSize(1);             // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);        // Draw white text
-  display.setCursor(0,0);             // Start at top-left corner
-  while(Serial2.available()>0){
-    if (gps.encode(Serial2.read()))
-      displayInfo();
-    if (millis() > 5000 && gps.charsProcessed() < 10)
-    {
-      display.println(F("No GPS detected: check wiring."));
+    vTaskDelay(500);
+    if (retry++ >= 20) { // timeout for connection is 10 seconds
+      display.println("Connection timeout expired! Start Smartconfig...");
       display.display();
-      while(true);
+      /* start SmartConfig */
+      WiFi.beginSmartConfig();
+      /* Wait for SmartConfig packet from mobile */
+      display.println(F("Waiting for SmartConfig."));
+      display.display();
+      while (!WiFi.smartConfigDone()) {
+        delay(500);
+        display.print(F("."));
+        display.display();
+      }
+      display.println(F(""));
+      display.println(F("SmartConfig done."));
+      /* Wait for WiFi to connect to AP */
+      display.println(F("Waiting for WiFi"));
+      display.display();
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        display.print(F("."));
+        display.display();
+      }
+      display.println(F("WiFi Connected."));
+      display.print(F("IP Address: "));
+      display.println(WiFi.localIP());
+      display.display();
+      delay(2000); // Pause for 2 seconds
     }
+    
   }
-  display.println(F("Hello, world!"));
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-  display.display();
 }
-
-
-void displayInfo()
-{
+void displayInfo(){
   display.clearDisplay();
   display.setTextSize(1);             // Normal 1:1 pixel scale
   display.setTextColor(SSD1306_WHITE);        // Draw white text
   display.setCursor(0,0);             // Start at top-left corner
   display.print(F("Location: ")); 
-  if (gps.location.isValid())
-  {
+  if (gps.location.isValid()){
     display.print(gps.location.lat(), 6);
     display.print(F(","));
     display.print(gps.location.lng(), 6);
-  }
-  else
-  {
+  }else{
     display.print(F("INVALID"));
   }
+  display.display();
 
   display.print(F("  Date/Time: "));
-  if (gps.date.isValid())
-  {
+  if (gps.date.isValid()){
     display.print(gps.date.month());
     display.print(F("/"));
     display.print(gps.date.day());
     display.print(F("/"));
     display.print(gps.date.year());
-  }
-  else
-  {
+  }else{
     display.print(F("INVALID"));
   }
+  display.display();
 
   display.print(F(" "));
-  if (gps.time.isValid())
-  {
+  if (gps.time.isValid()){
     if (gps.time.hour() < 10) display.print(F("0"));
     display.print(gps.time.hour());
     display.print(F(":"));
@@ -138,12 +123,36 @@ void displayInfo()
     display.print(F("."));
     if (gps.time.centisecond() < 10) display.print(F("0"));
     display.print(gps.time.centisecond());
-  }
-  else
-  {
+  }else{
     display.print(F("INVALID"));
   }
-
-  display.println();
   display.display();
+}
+
+void loop() {
+  display.clearDisplay();
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  int nsteps = 0;
+  while(nsteps < 30){
+    digitalWrite(s1step,HIGH);
+    delay(50);
+    digitalWrite(s1step,LOW);
+    delay(50);
+    nsteps++;
+  }
+  display.println("now, for the GPS!");
+  display.display();
+  while(SerialGPS.available()>0){
+    display.display();
+    if (gps.encode(SerialGPS.read()))
+      displayInfo();
+    if (millis() > 5000 && gps.charsProcessed() < 10){
+      display.println(F("No GPS detected: check wiring."));
+      display.display();
+      while(true);
+    }
+  }
+  delay(4000);
 }
